2b32874d8f8e4339e2c79ae599365f57c50f662a
Yan Bueno <buenoy519@gmail.com>
2025-12-03 10:12:19 -0300

    .

--- controllers/ControllerAgendamento.js
+++ controllers/ControllerAgendamento.js
@@ -15,85 +15,10 @@ module.exports = {
             // Recebe os dados do Form
             const { nomesAgendamento, horarioAgendamento, dataAgendamento, voucher, telefone } = req.body;
 
-            // Sanitização de Inputs
-
-            // →  Campo "nomesAgendamento"
-
-            // Verifica se "nomesAgendamento" existe e verifica typeof
-            if (!nomesAgendamento || typeof nomesAgendamento !== 'string') {
-                console.log('\n❌ Erro: [Preencha o campo "nomes" com uma string!]')
-                return res.status(400).render('errorPage', {
-                    error: `Preencha o campo 'nomes' com uma string!`
-                })
-            }
-
-            // Normaliza caracteres UniCode e aplica trim
-            let nomesTrim = nomesAgendamento.trim().normalize('NFC');
-
-            // Verifica length de "nomesAgendamento"
-            if (nomesTrim.length <= 2 || nomesTrim.length >= 255) {
-                console.log('\n❌ Erro: [Preencha o campo "Nomes" com valores entre 3-255 caracteres!]')
-                return res.status(400).render('errorPage', {
-                    error: `Preencha o campo 'Nomes' com valores entre 3-255 caracteres!`
-                });
-            }
-
-            // Regex que permite vírgulas e nomes com acento
-            const regexNomes = /^[A-Za-zÀ-ÖØ-öø-ÿ ,e]+$/
-
-            // Verifica se o valor de "nomesAgendamento" corresponde à regEx
-            if (!regexNomes.test(nomesTrim)) {
-                console.log('\n❌ Erro: [Formato inválido para o campo "Nomes". Volte e tente novamente!]')
-                return res.status(400).render('errorPage', {
-                    error: `Formato inválido para o campo 'Nomes'. Volte e tente novamente!`
-                })
-            }
-
-            // → Campo "horarioAgendamento"
-            // Verifica se existe e o tipo de "horarioAgendamento"
-            if (!horarioAgendamento || typeof horarioAgendamento !== 'string') {
-                console.error("Formato inválido para o campo 'Horário'. Volte e tente novamente!")
-                return res.status(400).render('errorPage', {
-                    error: `Formato inválido para o campo 'Horário'. Volte e tente novamente!`
-                })
-            }
-
-            // Valida se o horário segue o formato HH:mm (24h) → Rejeita 57:78, por exemplo 
-            let horarioValidado = moment(horarioAgendamento, 'HH:mm', true).isValid();
-            if (!horarioValidado) {
-                console.error("O horário não é valido. Volte e tente novamente!")
-                return res.status(400).render('errorPage', {
-                    error: `O horário não é válido. Volte e tente novamente!`
-                })
-            }
-
-            // Normaliza o horário para o formato HH:mm antes de salvar
-            let horarioBR = moment(horarioAgendamento, 'HH:mm').format('HH:mm');
-
-
-            // → Campo "dataAgendamento"
-            // Verifica se existe e seu tipo
-            if (!dataAgendamento || typeof dataAgendamento !== 'string') {
-                console.error(`Data inválida ou informação não enviada. Volte e tente novamente!`)
-                return res.status(400).render('errorPage', {
-                    error: `Data inválida ou informação não enviada. Volte e tente novamente!`
-                })
-            }
-
-            // Impede datas inválidas → 58/97/4798, por exemplo
-            let dataValidada = moment(dataAgendamento, 'YYYY-MM-DD', true).isValid();
-
-            if (!dataValidada) {
-                console.error(`Data inválida. Volte e tente novamente!`)
-                return res.status(400).render('errorPage', {
-                    error: `Data inválida. Volte e tente novamente!`
-                })
-            }
-
             // Cria o agendamento no Banco de Dados
             const agendamento = await Agendamento.create({
-                nomesAgendamento: nomesTrim,
-                horarioAgendamento: horarioBR,
+                nomesAgendamento,
+                horarioAgendamento,
                 dataAgendamento,
                 voucher,
                 telefone
@@ -172,9 +97,6 @@ module.exports = {
             })
         } catch (error) {
             console.error(error.message)
-            return res.render('errorPage', {
-                error
-            })
         }
     }
 }
